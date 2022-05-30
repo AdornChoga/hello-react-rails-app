437fb1872ccc1f867dde2a8969abacf3af9a8ff1
Adorn Choga <adornchoga@gmail.com>
2022-05-30 22:24:03 +0200

    Set up greetings

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::::GreetingsController < ApplicationController
+  def show
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+GREETINGS = [
+  {
+    :text => 'Hello'
+  },
+  {
+    :text => 'How are you?'
+  },
+  {
+    :text => "What's up?"
+  },
+  {
+    :text => 'How have you been?'
+  },
+  {
+    :text => 'You good?'
+  }
+]
+
+GREETINGS.each do |greeting|
+  Greeting.create(text: greeting[:text])
+end
