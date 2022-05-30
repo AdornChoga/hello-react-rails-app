a1d7fb5d3e492e50d8165a17ef39cf213fbdc17f
Adorn Choga <adornchoga@gmail.com>
2022-05-30 22:28:51 +0200

    Create greetings api

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,4 +1,7 @@
-class Api::V1::::GreetingsController < ApplicationController
+class Api::V1::GreetingsController < ApplicationController
   def show
+    render :json => {
+      :greeting => Greeting.all.sample()
+    }
   end
 end
